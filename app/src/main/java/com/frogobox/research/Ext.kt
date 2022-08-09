6b842c82a355d7653c7d9e8e9f01fc2e47362c75
amirisback <faisalamircs@gmail.com>
2022-08-09 20:41:44 +0700

    update: watch player

---
+++ app/src/main/java/com/frogobox/research/Ext.kt
@@ -0,0 +1,9 @@
+package com.frogobox.research
+
+import android.content.Context
+
+// Created by KoinWorks (M. Faisal Amir) on 09/08/22.
+
+fun Context.showToast(message: String) {
+    android.widget.Toast.makeText(this, message, android.widget.Toast.LENGTH_SHORT).show()
+}
--- app/src/main/java/com/frogobox/research/WatchActivity.kt
+++ app/src/main/java/com/frogobox/research/WatchActivity.kt
@@ -3,6 +3,7 @@ package com.frogobox.research
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
@@ -34,8 +35,12 @@ class WatchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
+        viewBinding.videoView.setOnClickListener {
+            showToast("Video View Clicked")
+        }
 
     }
+
 
     private fun setupExoPlayerByViewModel(exoPlayer: ExoPlayer, listener: Player.Listener) {
         mainViewModel.apply {
